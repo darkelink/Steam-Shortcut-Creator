9abbb743795a5fe21dd143097e2ef2bfdf8c37d4
darkelink <darkelink@gmail.com>
2015-08-21 08:41:15 +0100

    init

---
+++ Steam Shortcuts/SteamGame.cs
@@ -0,0 +1,54 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam_Shortcuts
+{
+    class SteamGame
+    {
+        public readonly int Id;
+        public string Name { get; set; }
+
+        protected string installDir;
+
+        protected static string steamDir = Properties.Settings.Default.steamDir;
+
+        public SteamGame(int Id, string Name, string installDir)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.installDir = installDir;
+        }
+
+        public string[] FindExecutables()
+        {
+            return Directory.GetFiles(getInstalledDir(), "*.exe");
+        }
+
+        public bool CreateShortcut(WshShell shell, string path, string icon)
+        {
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(path + @"\" + Name + ".lnk");
+            shortcut.Description = Name;
+            shortcut.TargetPath = steamDir + "steam.exe";
+            shortcut.Arguments = "applaunch " + Id;
+            shortcut.IconLocation = FindExecutables()[0];
+            shortcut.Save();
+            return true;
+        }
+
+        public bool CreateShortcut(WshShell shell, string path)
+        {
+            return CreateShortcut(shell, path, FindExecutables()[0]);
+        }
+
+        protected string getInstalledDir()
+        {
+            return steamDir + @"steamapps\common\" + installDir;
+        }
+
+    }
+}
