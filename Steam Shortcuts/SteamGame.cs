62e500b247f71129d2852b0ebdb358d094dcdf9a
darkelink <darkelink@gmail.com>
2015-08-21 14:35:45 +0100

    shortcut correct name

--- Steam Shortcuts/SteamGame.cs
+++ Steam Shortcuts/SteamGame.cs
@@ -15,18 +15,11 @@ namespace Steam_Shortcuts
 
         protected string installDir;
 
-        protected static string steamDir = Properties.Settings.Default.steamDir;
-
         public SteamGame(int Id, string Name, string installDir)
         {
             this.Id = Id;
             this.Name = Name;
             this.installDir = installDir;
-        }
-
-        public static void ChangeDir(string dir)
-        {
-            steamDir = dir;
         }
 
         public string[] FindExecutables()
@@ -36,23 +29,24 @@ namespace Steam_Shortcuts
 
         public bool CreateShortcut(WshShell shell, string path, string icon)
         {
-            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(path + @"\" + Name + ".lnk");
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(path + @"\" + installDir + ".lnk");
             shortcut.Description = Name;
-            shortcut.TargetPath = steamDir + "steam.exe";
-            shortcut.Arguments = "applaunch " + Id;
+            shortcut.TargetPath = Properties.Settings.Default.steamDir + @"\steam.exe";
+            shortcut.WorkingDirectory = Properties.Settings.Default.steamDir;
+            shortcut.Arguments = "-applaunch " + Id;
             shortcut.IconLocation = FindExecutables()[0];
             shortcut.Save();
             return true;
         }
 
-        public bool CreateShortcut(WshShell shell, string path)
+        public bool CreateShortcut(WshShell shell)
         {
-            return CreateShortcut(shell, path, FindExecutables()[0]);
+            return CreateShortcut(shell, Properties.Settings.Default.shortcutDir, FindExecutables()[0]);
         }
 
         protected string getInstalledDir()
         {
-            return steamDir + @"steamapps\common\" + installDir;
+            return Properties.Settings.Default.steamDir + @"\steamapps\common\" + installDir;
         }
 
         public override string ToString()
